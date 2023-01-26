bf9915ee421997a6871c172061d389cbb306cc49
Tomáš Stýblo <tomas.styblo@gmail.com>
2023-01-27 00:40:21 +0100

    uklizení funkcí do třídy Game_state

--- snake_game.py
+++ snake_game.py
@@ -6,6 +6,7 @@ SQUARE = 64 # v pixelech
 
 # velikost herního pole je 10 x 10
 window = pyglet.window.Window(width=SQUARE*10, height=SQUARE*10)
+pyglet.text.layout
 
 class Game_state:
     def __init__(self):
@@ -15,8 +16,37 @@ class Game_state:
         self.add_food()
         self.width = 10
         self.height = 10
-        self.move()
     
+    def move(self, t):
+        """Nastavení pohybu hada na základě použité klávesy od hráče (WSAD)"""
+        x, y = self.snake[-1]
+        if self.way == "right":
+            new_head = (x + 1, y) # úprava původní hlavy
+        elif self.way == "left":
+            new_head = (x - 1, y) # úprava původní hlavy
+        elif self.way == "up":
+            new_head = (x, y + 1) # úprava původní hlavy
+        elif self.way == "down":
+            new_head = (x, y - 1) # úprava původní hlavy
+
+        # had, když narazí do stěny, hra končí
+        (x, y) = new_head
+        if x < 0 or x > 9 or y < 0 or y > 9:
+            pyglet.clock.unschedule(self.move)
+
+        if new_head not in self.food:
+            del self.snake[0] # smazání první souřadnici
+
+        # had, když narazí do sebe sama, hra končí
+        if new_head in self.snake:
+            pyglet.clock.unschedule(self.move)
+
+
+        # pojídání jídla hadem
+        if new_head in self.food: # jidlo had
+            self.food.remove(new_head) # smaže jídlo hada
+            self.add_food() # přidá další náhodné jídlo pro hada
+        self.snake.append(new_head) # had se prodlouží o danou souřadnici s jídlem
 
     def add_food(self):
         """Přidá jídlo pro hada"""
@@ -30,51 +60,32 @@ class Game_state:
                     self.food.append((x, y))
                     break
 
-    def move(self):
-        """Nastavení pohybu hada na základě použité klávesy od hráče (WSAD)"""
-        (x, y) = self.snake[-1]
-        if self.way == "right":
-            new_head = (x + 1, y) # úprava původní hlavy
-        elif self.way == "left":
-            new_head = (x - 1, y) # úprava původní hlavy
-        elif self.way == "up":
-            new_head = (x, y + 1) # úprava původní hlavy
-        elif self.way == "down":
-            new_head = (x, y - 1) # úprava původní hlavy
+    #ovládání pomocí kláves "wsad" (změna atributu self.way pomocí kláves)
+    def button(self, text):
+        if text == "d":
+            game.way = "right"
+        elif text == "a":
+            game.way = "left"
+        elif text == "w":
+            game.way = "up"
+        elif text == "s":
+            game.way = "down"
 
-        if new_head not in self.food:
-            del self.snake[0] # smazání první souřadnici
-        # pojídání jídla hadem
-        elif new_head in self.food: # jidlo had
-            self.food.remove(new_head) # smaže jídlo hada
-            self.add_food() # přidá další náhodné jídlo pro hada
-        self.snake.append(new_head) # had se prodlouží o danou souřadnici s jídlem
-
-        body_snake = self.snake[0:-1] # tělo hada (bez hlavy)
-        # had, když narazí do stěny, hra končí
-        if x < 0 or x > 9 or y < 0 or y > 9:
-            pyglet.clock.unschedule(move)
-        # had, když narazí do sebe sama, hra končí
-        elif new_head in body_snake:
-            pyglet.clock.unschedule(move)
-
-
-
-
-pyglet.clock.schedule_interval(Game_state.move, 1/4)
-
-# ovládání pomocí kláves "wsad" (změna atributu self.way pomocí kláves)
-def button(text):
-    if text == "d":
-        game.way = "right"
-    elif text == "a":
-        game.way = "left"
-    elif text == "w":
-        game.way = "up"
-    elif text == "s":
-        game.way = "down"
+    def draw(self):
+        window.clear()
+        # vykreslení hada
+        for (x, y) in game.snake:
+            green_tile.x = x * SQUARE
+            green_tile.y = y * SQUARE
+            green_tile.draw()
+        # vykreslení jídla (jablka)
+        for (x, y) in game.food:
+            apple.x = x * SQUARE
+            apple.y = y * SQUARE
+            apple.draw()
 
 game = Game_state() # vytvoření objektu "game"
+pyglet.clock.schedule_interval(game.move, 1/4)
 
 # zelená dlaždice
 image_green = pyglet.image.load("tail.png")
@@ -84,22 +95,9 @@ green_tile = pyglet.sprite.Sprite(image_green)
 image_apple = pyglet.image.load("apple.png")
 apple = pyglet.sprite.Sprite(image_apple)
 
-def draw():
-    window.clear()
-    # vykreslení hada
-    for (x, y) in game.snake:
-        green_tile.x = x * SQUARE
-        green_tile.y = y * SQUARE
-        green_tile.draw()
-    # vykreslení jídla (jablka)
-    for (x, y) in game.food:
-        apple.x = x * SQUARE
-        apple.y = y * SQUARE
-        apple.draw()
-        
 window.push_handlers(
-    on_text=button,
-    on_draw=draw
+    on_text=game.button,
+    on_draw=game.draw
     )
 
 pyglet.app.run()
